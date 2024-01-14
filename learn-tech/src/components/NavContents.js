cfee1b92857e1a59cc0240b39d5d34c80b3bae5b
Prince_Palewar <princepalewar12@gmail.com>
2024-01-14 21:06:57 +0530

    created navbar for content

--- learn-tech/src/components/NavContents.js
+++ learn-tech/src/components/NavContents.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const NavContents = () => {
   return (
-    <div>NavContents</div>
-  )
-}
+    <>
+      <div className="nav-container ">
+        <ul className="flex justify-between px-8 py-2 border-y-4 border-y-black bg-slate-600 text-white ">
+          <li className="text-2xl font-semibold">React</li>
+          <li className="text-2xl font-semibold">JavaScript</li>
+          <li className="text-2xl font-semibold">Tailwindcss</li>
+          <li className="text-2xl font-semibold">BootStrap</li>
+          <li className="text-2xl font-semibold">CSS</li>
+          <li className="text-2xl font-semibold">HTML</li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
-export default NavContents
+export default NavContents;
